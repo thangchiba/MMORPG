d0a80602c96362bb2db8d387253ade784063f24c
HoangThang <thangchiba@gmail.com>
2022-03-21 19:28:11 +0900

    Chasing And Attack Target

--- Assets/Scripts/Combat/Fight.cs
+++ Assets/Scripts/Combat/Fight.cs
@@ -2,18 +2,20 @@ using System;
 using System.Collections;
 using MMORPG.Move;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace MMORPG.Combat
 {
     public class Fight : MonoBehaviour
     {
-        [SerializeField] float attackRange = 5f;
+        [SerializeField] float attackRange = 3f;
         GameObject target;
         bool attackMode = false;
         bool isAttacking = false;
         bool isChasing = false;
         public void SetTarget(GameObject target)
         {
+            GetComponent<NavMeshAgent>().stoppingDistance = attackRange;
             attackMode = false;
             this.target = target;
             if (target.tag == "Enemy" || target.tag == "Minion")
@@ -21,12 +23,6 @@ namespace MMORPG.Combat
                 attackMode = true;
                 StartCoroutine("Attack");
             }
-        }
-        private void ChaseTarget()
-        {
-            if(!isChasing)
-            GetComponent<Mover>().MoveToPoint(target.gameObject.transform.position);
-            isChasing = true;
         }
 
         IEnumerator Attack()
@@ -46,6 +42,13 @@ namespace MMORPG.Combat
                 }
                 yield return new WaitForSeconds(1);
             }
+        }
+
+        private void ChaseTarget()
+        {
+            if (!isChasing)
+                GetComponent<Mover>().MoveToPoint(target.gameObject.transform.position);
+            isChasing = true;
         }
     }
 }
