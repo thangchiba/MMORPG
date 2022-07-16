f58001f6a85e2f205ebc7da9d28e5f58451d96d3
ThangChiba <thangchiba@gmail.com>
2022-07-16 22:00:40 +0900

    Add HealthBar

--- Assets/Scripts/GUI/DamageTextContainer.cs
+++ Assets/Scripts/GUI/DamageTextContainer.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
 
 public class DamageTextContainer : MonoBehaviour
 {
     [SerializeField] GameObject DamageText = null;
-    [SerializeField] TextMesh text;
 	public void Spawn(float damage)
     {
         GameObject instance = Instantiate(DamageText, gameObject.transform);
-        Debug.Log("take damage : "+damage);
         instance.GetComponentInChildren<DamageText>().SetDamageText(damage);
     }
 }
