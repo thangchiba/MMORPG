1173db8b0ac06de182e195984539c4856a656797
ThangChiba <thangchiba@gmail.com>
2022-07-19 23:13:39 +0900

    Add Ear and Play Audio on Awake

---
+++ Assets/Scripts/Common/PlayAudioSourceOnAwake.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MMORPG.Common
+{
+    public class PlayAudioSourceOnAwake : MonoBehaviour
+    {
+        [SerializeField] AudioSource audio = null;
+        [SerializeField] float destroyAfter = 5f;
+        void Awake()
+        {
+            if (audio != null)
+            {
+                audio.Play();
+                Destroy(audio, destroyAfter);
+            }
+        }
+    }
+
+}
